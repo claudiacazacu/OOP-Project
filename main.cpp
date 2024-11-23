0a24c3aa3c60338b97ec3d6e2cbab0ce6b69225e
claudiacazacu <claudia-mihaela.cazacu@s.unibuc.ro>
2024-11-23 22:44:43 +0200

    milestone1

--- main.cpp
+++ main.cpp
@@ -1,57 +1,151 @@
 #include <iostream>
-#include <array>
+#include <vector>
+#include <string>
 
-#include <Helper.h>
+class Attack {
+private:
+    std::string name;
+    int power;
+
+public:
+    Attack(const std::string& name, int power) : name(name), power(power) {}
+
+    ~Attack() {
+       //  std::cout << "Destructorul Attack a fost apelat\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Attack& attack) {
+        os << attack.name << " (Power: " << attack.power << ")";
+        return os;
+    }
+
+    int getPower() const { return power; }
+};
+
+class Pokemon {
+private:
+    std::string name;
+    int hp;
+    std::vector<Attack> attacks;
+
+public:
+    Pokemon(const std::string& name, int hp, const std::vector<Attack>& attacks)
+        : name(name), hp(hp), attacks(attacks) {}
+
+    ~Pokemon() {
+        // std::cout << "Destructorul Pokemon a fost apelat\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Pokemon& pokemon) {
+        os << pokemon.name << " (HP: " << pokemon.hp << ")";
+        return os;
+    }
+
+    void showAttacks() const {
+        for (const auto& attack : attacks) {
+            std::cout << attack << "\n";
+        }
+    }
+
+    void receiveDamage(int damage) {
+        hp -= damage;
+        if (hp < 0) hp = 0;
+    }
+
+    int getHp() const { return hp; }
+    std::string getName() const { return name; }
+
+    const Attack& chooseAttack(int index) const {
+        if (index >= 0 && index < attacks.size()) {
+            return attacks[index];
+        }
+        return attacks[0];
+    }
+};
+
+class Trainer {
+private:
+    std::string name;
+    Pokemon pokemon;
+
+public:
+    Trainer(const std::string& name, const Pokemon& pokemon) : name(name), pokemon(pokemon) {}
+
+    ~Trainer() {
+       // std::cout << "Destructorul Trainer a fost apelat\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Trainer& trainer) {
+        os << "Trainer: " << trainer.name << " with " << trainer.pokemon;
+        return os;
+    }
+
+    void instructAttack(Trainer& opponent) {
+        int damage = pokemon.chooseAttack(0).getPower();
+        opponent.pokemon.receiveDamage(damage);
+    }
+};
+
+class Battle {
+private:
+    Trainer& trainer1;
+    Trainer& trainer2;
+
+public:
+    Battle(Trainer& trainer1, Trainer& trainer2) : trainer1(trainer1), trainer2(trainer2) {}
+
+    void start() {
+        std::cout << "Battle Start: " << trainer1 << " VS " << trainer2 << "\n";
+        trainer1.instructAttack(trainer2);
+        trainer2.instructAttack(trainer1);
+        std::cout << trainer1 << "\n" << trainer2 << "\n";
+    }
+};
+
+Pokemon choosePokemon() {
+    std::vector<Pokemon> availablePokemon = {
+        Pokemon("Pikachu", 100, { Attack("Thunderbolt", 50), Attack("Quick Attack", 20), Attack("Iron Tail", 40) }),
+        Pokemon("Charmander", 100, { Attack("Flamethrower", 55), Attack("Scratch", 15), Attack("Ember", 30) }),
+        Pokemon("Bulbasaur", 100, { Attack("Vine Whip", 45), Attack("Tackle", 15), Attack("Razor Leaf", 35) })
+    };
+
+    std::cout << "Cu ce Pokemon vrei sa joci?\n";
+    for (size_t i = 0; i < availablePokemon.size(); ++i) {
+        std::cout << i + 1 << ". " << availablePokemon[i].getName() << " cu atacurile:\n";
+        availablePokemon[i].showAttacks();
+    }
+
+    int choice;
+    std::cout << "Alege numarul Pokemonului: ";
+    std::cin >> choice;
+
+    while (choice < 1 || choice > availablePokemon.size()) {
+        std::cout << "Alegere invalida. Alege iar: ";
+        std::cin >> choice;
+    }
+
+    return availablePokemon[choice - 1];
+}
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+    std::string trainer1Name, trainer2Name;
+    std::cout << "Nume trainer 1: ";
+    std::cin.ignore();
+    std::getline(std::cin, trainer1Name);
+
+    std::cout << "Nume trainer 2: ";
+    std::getline(std::cin, trainer2Name);
+
+    std::cout << trainer1Name << ", Nr pokemonului pe care l vrei:\n";
+    Pokemon pokemon1 = choosePokemon();
+    Trainer trainer1(trainer1Name, pokemon1);
+
+    std::cout << trainer2Name << ", Nr pokemonului pe care l vrei:\n";
+    Pokemon pokemon2 = choosePokemon();
+    Trainer trainer2(trainer2Name, pokemon2);
+
+    Battle battle(trainer1, trainer2);
+    battle.start();
+
     return 0;
 }
